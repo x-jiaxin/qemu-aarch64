1451108be7f3a8b9f0c4dfb7d699adc1a5387741
xingjiaxin <jiaxin.cuckoo@gmail.com>
2025-02-16 16:48:37 +0800

    first commit

---
+++ qemu_arm.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+qemu-system-arm \
+        -M vexpress-a9  \
+        -m 512M \
+        -kernel $HOME/Code/linux/linux-6.13.2/arch/arm/boot/zImage   \
+        -dtb $HOME/Code/linux/linux-6.13.2/arch/arm/boot/dts/arm/vexpress-v2p-ca9.dtb    \
+        -append "root=/dev/mmcblk0 rw console=ttyAMA0" \
+        -sd rootfs-arm32.ext3 \
+        -nographic
