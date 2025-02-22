476418c256ac75bef0af2e4b8c1dec88cedad7ee
xingjiaxin <jiaxin.cuckoo@gmail.com>
2025-02-22 17:23:27 +0800

    qemu gdb debug config.

--- qemu_aarch64.sh
+++ qemu_aarch64.sh
@@ -6,6 +6,7 @@ qemu-system-aarch64 \
     -m 512M \
     -kernel "$HOME"/Code/linux/linux-6.13.2/arch/arm64/boot/Image \
     -initrd "$HOME"/Code/busybox/busybox-1.36.1/initramfs.cpio.gz \
-    -append "rdinit=/linuxrc root=/dev/vda rw console=ttyAMA0" \
+    -append "nokaslr rdinit=/linuxrc root=/dev/vda rw console=ttyAMA0" \
     -drive format=raw,file=./rootfs-aarch64.ext3 \
-    -nographic
+    -nographic \
+    -S -s
--- qemu_sync_to_rootfs.sh
+++ qemu_sync_to_rootfs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 sudo mount -t ext3 rootfs-aarch64.ext3 /mnt/rootfs -o loop
+sudo rm -rf ./rootfs-aarch64/*
 sudo cp -rf /mnt/rootfs/* rootfs-aarch64
 sudo umount /mnt/rootfs
